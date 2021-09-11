ef87a7ffbc0e824b81e0741d74cde24f4a5339cb
Satyanchal <62104921+Bluehatcoders@users.noreply.github.com>
2021-09-11 18:24:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,24 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+const ProtonMail = require('protonmail-api');
+
+(async () => {
+  const pm = await ProtonMail.connect({
+    username: 'undefined127001@protonmail.com',
+    password: 'satyanchalundefined'
+  })
+
+  await pm.sendEmail({
+    to: 'quickhalewebtech@gmail.com',
+    subject: 'Send email tutorial',
+    body: 'Hello world'
+  })
+
+  pm.close()
+})()
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Bhai mail sent');
 });
 
 app.listen(PORT, HOST);
